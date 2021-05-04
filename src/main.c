20bc84c1f270a3f19bb2dec9ba0319a71067cbc1
Vladimir Panteleev <git@cy.md>
2021-05-04 22:27:27 +0000

    src/main.c: Adjust mmap rule for bionic

--- src/main.c
+++ src/main.c
@@ -140,11 +140,10 @@ static void enable_seccomp(int argc, char *argv[]) {
     goto out;
 
   /* Allow allocating memory */
-  rc = seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(mmap), 5,
+  rc = seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(mmap), 4,
     SCMP_A0(SCMP_CMP_EQ, (size_t)NULL),
     SCMP_A2(SCMP_CMP_EQ, PROT_READ | PROT_WRITE),
     SCMP_A3(SCMP_CMP_EQ, MAP_PRIVATE | MAP_ANONYMOUS),
-    SCMP_A4(SCMP_CMP_EQ, -1),
     SCMP_A5(SCMP_CMP_EQ, 0));
   if (rc < 0)
     goto out;
